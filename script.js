2d746348a29a416f0c58eb8b264c9cd9f8665f86
Estudante 2 <Valter de Sales>
2025-08-19 16:22:50 -0300

    novas coisinhas

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let cont = 0
-let NumR = parseInt(Math.random() * 100)
-
-
+let NumR = parseInt(Math.random() * 20)
+const lista = []
+const button = document.getElementById("but")
 
 function contador(){
     cont+=1
     document.getElementById("contador").textContent = "Tentativas: " + cont
+
+    //modificar, quando chegar a 10 tentativas tem que zerar e aparecer um botão com novo jogo
+    
+    /* if(cont == 9){
+        document.getElementById("error").textContent = "Estorou as tentativas. Tente Novamente"
+    } */ 
 }
 
 function NumRandom(){
     let input = parseInt(document.getElementById("inp").value)
     if (input != NumR){
-        let lista = []
-        let newL = lista.push(input)
-        for(i in lista){
-            document.getElementById("NumUtilizados").textContent = "Já foram utilizados: " + lista[i]
-        }
+        lista.push(input)
+        const listalterada = Array.from(new Set(lista))
+        document.getElementById("NumUtilizados").textContent = "Já foram utilizados: " + listalterada.join(", ")
         if(input> NumR){
             document.getElementById("error").textContent = "Errou, tente novamente. O numero é menor." + NumR
         }else{
