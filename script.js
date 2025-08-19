e3a3ae93e5f70885dd69c1cb7d6a897482facb98
snooss <luanzins.me@gmail.com>
2025-08-19 16:29:00 -0300

    Branch lucas ai

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ function contador(){
     document.getElementById("contador").textContent = "Tentativas: " + cont
 }
 
+function Jogarnovamente(){
+    let input = parseInt(document.getElementById("inp").value)
+    input = ""
+    document.getElementById("NumUtilizados").textContent = ""
+    document.getElementById("error").textContent = ""
+    document.getElementById("contador").textContent = "Tentativas: " + 0
+}
+
 function NumRandom(){
     let input = parseInt(document.getElementById("inp").value)
     if (input != NumR){
@@ -22,6 +30,8 @@ function NumRandom(){
             document.getElementById("error").textContent = "Errou, tente novamente. O numero é maior." + NumR
         }
     } else{
-        document.getElementById("error").textContent = "Acertou, uhuuuuuuul"
+        document.getElementById("error").textContent = "Acertou, uhuuuuuuul." 
+
     }
 }
+
