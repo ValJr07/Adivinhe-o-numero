8c0dec3dbe623d188c2bbf97d21c47cd31716576
ValJr07 <vsalesjr2008@gmail.com>
2025-08-21 09:47:23 -0300

    add jogarnovamente

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ let cont = 0
 let NumR = parseInt(Math.random() * 20)
 const lista = []
 const button = document.getElementById("but")
+const stat = document.getElementById("error")
+const JogarN = document.getElementById("JogarN")
 
 function contador(){
     cont+=1
     document.getElementById("contador").textContent = "Tentativas: " + cont
 
     //modificar, quando chegar a 10 tentativas tem que zerar e aparecer um botão com novo jogo
-    
-    /* if(cont == 9){
-        document.getElementById("error").textContent = "Estorou as tentativas. Tente Novamente"
-    } */ 
+    if(cont == 10){
+            JogarN.style.display = "block"
+    }
+}
+
+function Jogarnovamente(){
+    let input = parseInt(document.getElementById("inp").value)
+    input = ""
+    document.getElementById("NumUtilizados").textContent = ""
+    document.getElementById("error").textContent = ""
+    document.getElementById("contador").textContent = "Tentativas: " + 0
 }
 
 function NumRandom(){
@@ -21,11 +30,12 @@ function NumRandom(){
         const listalterada = Array.from(new Set(lista))
         document.getElementById("NumUtilizados").textContent = "Já foram utilizados: " + listalterada.join(", ")
         if(input> NumR){
-            document.getElementById("error").textContent = "Errou, tente novamente. O numero é menor." + NumR
+            stat.textContent = "Errou, tente novamente. O numero é menor." + NumR
         }else{
-            document.getElementById("error").textContent = "Errou, tente novamente. O numero é maior." + NumR
+            stat.textContent = "Errou, tente novamente. O numero é maior." + NumR
         }
     } else{
         document.getElementById("error").textContent = "Acertou, uhuuuuuuul"
+        
     }
 }
